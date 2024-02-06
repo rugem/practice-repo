60365b4ce45966799169af880caf9c12ab5d42e1
Mat <21244966+rugem@users.noreply.github.com>
2024-02-06 17:01:06 -0500

    Work in progress, fetched pet data

--- main.js
+++ main.js
@@ -14,3 +14,16 @@ async function start() {
 }
 
 start()
+
+async function petsArea() {
+  const petsPromise = await fetch(
+    'https://learnwebcode.github.io/bootcamp-pet-data/pets.json'
+  )
+  const petsData = await petsPromise.json()
+  petsData = petsData.forEach(pet => {
+    console.log(pet.name);
+  });
+
+}
+
+petsArea()
