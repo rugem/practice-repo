f5177453f305f733cad392549b251ccf6dd03437
Mat <21244966+rugem@users.noreply.github.com>
2024-02-09 13:27:45 -0500

    Finished pet filtering

--- main.js
+++ main.js
@@ -26,14 +26,17 @@ async function petsArea() {
   petsData.forEach((pet) => {
     const clone = template.content.cloneNode(true)
 
+    clone.querySelector('.pet-card').dataset.species = pet.species
     clone.querySelector('h3').textContent = pet.name
     clone.querySelector('.pet-description').textContent = pet.description
     clone.querySelector('.pet-age').textContent = createAgeText(pet.birthYear)
 
-    if (!pet.photo) pet.photo = "images/fallback.jpg"
+    if (!pet.photo) pet.photo = 'images/fallback.jpg'
 
     clone.querySelector('.pet-card-photo img').src = pet.photo
-    clone.querySelector('.pet-card-photo img').alt = `A ${pet.species} named ${pet.name}.`
+    clone.querySelector(
+      '.pet-card-photo img'
+    ).alt = `A ${pet.species} named ${pet.name}.`
 
     wrapper.appendChild(clone)
   })
@@ -46,25 +49,33 @@ petsArea()
 function createAgeText(birthYear) {
   const currentYear = new Date().getFullYear()
   const age = currentYear - birthYear
-  
-  if (age === 1) return "1 year old";
-  if (age === 0) return "Less than a year old";
+
+  if (age === 1) return '1 year old'
+  if (age === 0) return 'Less than a year old'
 
   return `${age} years old`
 }
 
 // pet filter button code
 const allButtons = document.querySelectorAll('.pet-filter button')
-allButtons.forEach(el => {
+allButtons.forEach((el) => {
   el.addEventListener('click', handleButtonClick)
 })
 
 function handleButtonClick(e) {
   // remove the active class from any and all buttons
-  allButtons.forEach(el => el.classList.remove('active'))
+  allButtons.forEach((el) => el.classList.remove('active'))
 
   // add the active class to the specific button that just got clicked
   e.target.classList.add('active')
 
   // actually filter the pets down below
+  const currentFilter = e.target.dataset.filter
+  document.querySelectorAll('.pet-card').forEach((el) => {
+    if (currentFilter == el.dataset.species || currentFilter == 'all') {
+      el.style.display = 'grid'
+    } else {
+      el.style.display = 'none'
+    }
+  })
 }
